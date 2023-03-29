38eed5309bc69412a742f4714da0c99150d2bef6
endlingzyb <52320305+endlingzyb@users.noreply.github.com>
2023-03-29 18:36:41 +0800

    Update centos7-init.sh

--- centos7-init.sh
+++ centos7-init.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum clean all
 yum makecache
 yum update -y
-yum install -y sudo wget git crontabs
+yum install -y sudo git crontabs
 adduser -m -g -p  wheel xxx
 gti clone https://gitee.com/fenei/wondershaper.git
 cd wondershaper
 make install
-passwd zzz
+passwd xxx
